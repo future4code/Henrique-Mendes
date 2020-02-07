2a73bea158cf8619a7f9b62715276b51ec152c3d
Henrique Mendes <hdmendes88@gmail.com>
2020-02-07 11:18:35 -0300

    Exercicios de interpretalcao de codigo

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,41 @@
+// ------- Exercícios de interpretação de código -------
+//  ---------------------- 1 ---------------------- 
+/* Uma função que converte o valor da moeda de Dolar para Real.
+O usuario informa a cotação pelo prompt e a função multiplica a cotação pelo valor do parametro da função.
+No exemplo será 100*cotação informada e a função ira retornar R$ resultado
+Visualização do console:
+promt: "Informe o valor da cotação do Dólar"
+*/
+
+//  ---------------------- 2 ---------------------- 
+/* A função calcula o retorno do investimento usando 2 parametros (string e numerico).
+Primeiro é criado uma variavel para armazenar o montante final (valorAposInvestimento)
+Com a estrutura switch, a função testa a combinação do primeiro parametro (string) com as combinações possiveis na ordem escrita,
+parando o teste caso a condição seja alcançada. Em caso negativo, seja imetido um alert de tipo de investimento incorreto.
+Um vez a condição satisfeita, a função ira fazer a formula escrita e retornar o novo valor da variavel do montante final (valorAposInvestimento).
+Visualização do console:
+165
+TIPO DE INVESTIMENTO INFORMADO INCORRETO!
+*/
+
+//  ---------------------- 3 ---------------------- 
+/* Com o comando for of, será percorrido toda a extensão da array numeros
+Se o valor do elemento for par, então sera colocado no final da array1, caso contrario sera colocado no final da array2.
+Visualização do console:
+Quantidade total de números, 14
+6
+8
+*/
+
+//  ---------------------- 4 ----------------------
+/* Com o for of, na array numeros, o sistema ira ler todos os valores dos indeces dessa array.
+O primeiro if ira testar se algum valor é menor que o let numero1 = infinito, e se for então ele sera o novo valor do numero1.
+O segundo if testa se o ha algum valor acima de 0, caso haja, então o novo valor da let numero2 sera o valor desse elemento.
+Visualização do console:
+-10
+1590
+*/
+
+//  ---------------------- 1 ---------------------- 
+
+//  ---------------------- 1 ---------------------- 
