0cd14b913241fbdc9b8cbf6e7e1967ca46893795
Henrique Mendes <hdmendes88@gmail.com>
2020-04-28 23:10:45 -0300

    exercicio aula 2 semana15

---
+++ semana15/aula2/Saler.js
@@ -0,0 +1,10 @@
+// import { User } from "./User";
+// export class Saler extends User{
+//     commission: number;
+//     constructor(commission: number){
+//         this.commission = commission;
+//     }
+//     setComission(commission): number{
+//         return commission*2
+//     }
+// }
